160259d9a83729212d73e4fa4b4ea5d4203e4f2b
Jason Chavannes <jason.chavannes@gmail.com>
2025-10-28 14:35:18 -0700

    Unified example

--- example/main.go
+++ example/main.go
@@ -3,10 +3,9 @@ package main
 import (
 	"log/slog"
 	"os"
-	"strconv"
 
+	"github.com/bsv-blockchain/go-overlay-fiber/example/services/any"
 	"github.com/bsv-blockchain/go-overlay-fiber/pkg/server"
-	"github.com/bsv-blockchain/go-overlay-services/pkg/core/engine"
 )
 
 // Hi there! Let's configure Go Overlay Fiber!
@@ -23,16 +22,11 @@ func main() {
 		// Provide the HTTPS URL where your node is available on the internet
 		os.Getenv("HOSTING_URL"),
 	)
-	var port int
-	if envPort := os.Getenv("PORT"); envPort != "" {
-		port, _ = strconv.Atoi(envPort)
-	} else {
-		port = 8081
-	}
 
 	slog.SetLogLoggerLevel(slog.LevelInfo)
 
-	overlayServer.ConfigurePort(port)
+	// Decide what port you want the server to listen on.
+	overlayServer.ConfigurePort(8080)
 
 	// Connect to MySQL database
 	if mysqlURL := os.Getenv("MYSQL_URL"); mysqlURL != "" {
@@ -47,32 +41,19 @@ func main() {
 		overlayServer.ConfigureMongoDB(mongoURL)
 	}
 
-	var engineConfig server.EngineConfig
-	if shipTracker := os.Getenv("SHIP_TRACKER"); shipTracker != "" {
-		engineConfig.ShipTrackers = []string{shipTracker}
-	}
-	if slapTracker := os.Getenv("SLAP_TRACKER"); slapTracker != "" {
-		engineConfig.SlapTrackers = []string{slapTracker}
-	}
-	engineConfig.SyncConfiguration = map[string]engine.SyncConfiguration{
-		"tm_ship": {Type: engine.SyncConfigurationPeers, Peers: []string{"http://localhost:8080"}},
-		"tm_slap": {Type: engine.SyncConfigurationPeers, Peers: []string{"http://localhost:8080"}},
-	}
-	overlayServer.ConfigureEngineParams(engineConfig)
-
 	// Here, you will configure the overlay topic managers and lookup services you want.
 	// - Topic managers decide what outputs can go in your overlay
 	// - Lookup services help people find things in your overlay
-	// - Make use of functions like `ConfigureTopicManager` and `ConfigureLookupServiceWithMongo`
-	// ADD YOUR OVERLAY SERVICES HERE
+
+	// Any
+	overlayServer.ConfigureTopicManager("tm_anytx", any.NewAnyTopicManager())
+	overlayServer.ConfigureLookupService("ls_anytx", any.NewAnyLookupService(overlayServer.MongoDB))
 
 	// Enable GASP sync for testing
 	overlayServer.ConfigureGASPSync(true)
 
 	// Configure the engine with auto-configuration for SHIP/SLAP services
 	overlayServer.ConfigureEngine(true)
-
-	slog.Info("Starting server", "port", port)
 
 	// Lastly, start the server!
 	if err := overlayServer.Start(); err != nil {
